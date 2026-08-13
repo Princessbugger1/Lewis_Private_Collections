3470cdf02876cf2a35d83553665ff4a93e8fb486
Princessbugger1 <anna@lewispropainting.com>
2026-08-13 19:51:43 -0400

    Cache the isolated catalog visibility helper

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lewis-private-collections-v6';
-const ASSETS=['./','./index.html','./manifest.json','./catalog-enhancements.js'];
+const CACHE='lewis-private-collections-v7';
+const ASSETS=['./','./index.html','./manifest.json','./catalog-enhancements.js','./catalog-view-fix.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith((async()=>{
