8b3eec3f10af5bc505b23b899382baac20fcc0d1
Princessbugger1 <anna@lewispropainting.com>
2026-08-13 20:59:14 -0400

    Refresh service worker cache for current enhancements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lewis-private-collections-v7';
+const CACHE='lewis-private-collections-v8';
 const ASSETS=['./','./index.html','./manifest.json','./catalog-enhancements.js','./catalog-view-fix.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
